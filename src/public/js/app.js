853bb6c76bdf43a4f20cf5d793332114c22ad39a
gwuiinkim <gwuiinkim@gmail.com>
2021-08-28 16:09:38 +0900

    Chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,25 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 // here, socket means connected server..
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", ()=> {
-    console.log("Connected to Server");
+socket.addEventListener("open", () => {
+  console.log("Connected to Server");
 });
 
-socket.addEventListener("message", (message)=> {
-    console.log("Just got this: ",message.data, "from the server");
+socket.addEventListener("message", message => {
+  console.log("Just got this: ", message, "from the server");
 });
 
-socket.addEventListener("close", ()=> {
-    console.log("Disconnected from Server");
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server");
 });
 
-setTimeout(()=> {
-    socket.send("hello from the browser!");
-}, 3000)
+messageForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  console.log(input.value);
+  input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -6,27 +6,29 @@ import { createSocket } from "dgram";
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname  + "/views");
-app.use("/public", express.static(__dirname+"/public"));
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000 and ws://localhost:3000`);
+const handleListen = () =>
+  console.log(`Listening on http://localhost:3000 and ws://localhost:3000`);
 
 // starting http, websocket server in the same server
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 
+const sockets = [];
 
-wss.on("connection", (socket) => {
-    // here, socket means connected browser.
-    console.log("Connected to Browser");
-    socket.send("hello!!");
-    socket.on("close", () => console.log("Disconnected from the Browser"));
-    socket.on("message", (message) => {
-        console.log(message.toString('utf-8'));
-    })
+wss.on("connection", socket => {
+  sockets.push(socket);
+  // here, socket means connected browser.
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", message => {
+    const strMessage = message.toString("utf-8");
+    sockets.forEach(aSocket => aSocket.send(strMessage));
+  });
 });
-
 
 server.listen(3000, handleListen);
