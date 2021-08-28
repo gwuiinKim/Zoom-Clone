febf731bc156396b9238f469a77e3cbe2e6e8988
gwuiinkim <gwuiinkim@gmail.com>
2021-08-28 14:54:38 +0900

    WebSocket Events

--- src/server.js
+++ src/server.js
@@ -16,4 +16,10 @@ const handleListen = () => console.log(`Listening on http://localhost:3000 and w
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
+function handleConnection(socket) {
+    // here, socket means connected browser
+    console.log(socket);
+  }
+wss.on("connection", handleConnection);
+
 server.listen(3000, handleListen);
